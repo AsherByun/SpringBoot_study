19934e51dd86074b97c0c0d0cdedd1e5e861a591
변성재 <dfdo333@naver.com>
2021-01-08 00:59:00 +0900

    work done

--- src/main/java/com/study/view/controller/SignUpController.java
+++ src/main/java/com/study/view/controller/SignUpController.java
@@ -1,4 +1,4 @@
-package com.study.controller;
+package com.study.view.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SignUpController {
 	
-	@GetMapping("/signup")
+	@GetMapping(path = "/signUp")
 	public String signUpPage() {
-		return "signUpAge";
+		return "home";
 	}
 }
